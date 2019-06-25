e65322898e4b27f96868b7d699f513be6273f5cd
Josh Day <jwday@ucdavis.edu>
2019-06-25 14:30:20 -0700

    new code and burn time stuff

---
+++ Thrust_vs_BurnTime.py
@@ -0,0 +1,31 @@
+# Thrust vs. Burn Time for 0.25 N-s impulse requirement 
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 60, 6001)
+
+req_impulse = 0.25  # N-s
+max_time = 56  # Seconds
+min_const_thrust = req_impulse/max_time  # N
+
+num_thrust_pulses = 4
+thrust_pulse_impulse = req_impulse/num_thrust_pulses  # This is equivalent to the area under the curve
+pulse_startstop_times = np.linspace(0, max_time, num_thrust_pulses*2)
+np.append(pulse_startstop_times, [60])
+
+# Assume pulses decrease linearly in thrust over time (triangular pulse)
+thrust_pulse_duration = max_time/(2*num_thrust_pulses)  # Assuming plenum refil is equal in time to pulse duration
+max_pulse_thrust = 2*thrust_pulse_impulse/thrust_pulse_duration  # The "height" of the pulse triangle
+
+pulse_thrust = []
+for i in t:
+    n = 0
+    if n < 2*num_thrust_pulses-1:
+        if i > pulse_startstop_times[n] and i < pulse_startstop_times[n+1]:
+            pulse_thrust.append( i*(-max_pulse_thrust/thrust_pulse_duration) + max_pulse_thrust*(2*n+1))
+        elif i > pulse_startstop_times[n+1] and i < pulse_startstop_times[n+2]:
+            pulse_thrust.append(0)
+        else:
+            n += 1
+
+plt.plot(t, pulse_thrust)
